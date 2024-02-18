75a42538afb9327064f4c9f801868edb37666229
Denis0872 <denispetergof@gmail.com>
2024-02-18 17:23:10 +0300

    111

--- src/org/example/Animal.java
+++ src/org/example/Animal.java
@@ -2,6 +2,11 @@ package org.example;
 
 public class Animal implements Everything{
     @Override
+    public String toString() {
+        return "Animal{}";
+    }
+
+    @Override
     public void run() {
         System.out.println("это класс эномал");
     }
